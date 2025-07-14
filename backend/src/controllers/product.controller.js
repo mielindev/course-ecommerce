3d820efd70778260087e7ecf7b42d6067ad220d9
mielindev <phamvietquang.dev@gmail.com>
2025-07-14 20:22:02 +0700

    ✨ feat(product): implement product filtering and pagination
    - add search functionality to filter products by name/description
    - add category filter to retrieve products by category
    - add price filter options to filter products by price range
    - implement pagination to retrieve products in chunks

--- backend/src/controllers/product.controller.js
+++ backend/src/controllers/product.controller.js
@@ -12,23 +12,68 @@ const productController = {
 
   getProducts: async (req, res) => {
     try {
-      const page = Math.max(1, parseInt(req.query.page) || 1);
-      const pageSize = Math.max(1, parseInt(req.query.pageSize) || 10);
-      const skip = (page - 1) * pageSize;
+      const {
+        search,
+        category,
+        priceOption,
+        page = 1,
+        pageSize = 10,
+      } = req.query;
+
+      const filter = {};
+
+      if (search) {
+        filter.$or = [
+          { name: { $regex: search, $options: "i" } },
+          { description: { $regex: search, $options: "i" } },
+          { shortDescription: { $regex: search, $options: "i" } },
+        ];
+      }
+
+      if (category) {
+        filter.category = category;
+      }
+
+      if (priceOption) {
+        filter.price = {};
+        switch (priceOption) {
+          case "below500":
+            filter.price.$lt = 500000;
+            break;
+          case "from500to1000":
+            filter.price.$gte = 500000;
+            filter.price.$lte = 1000000;
+            break;
+          case "over1000":
+            filter.price.$gt = 1000000;
+            break;
+          default:
+            break;
+        }
+      }
+
+      const skip = (parseInt(page) - 1) * parseInt(pageSize);
+
       const [products, total] = await Promise.all([
-        Product.find().skip(skip).limit(pageSize).populate("category"),
-        Product.countDocuments(),
+        Product.find(filter)
+          .skip(skip)
+          .limit(parseInt(pageSize))
+          .populate("category")
+          .exec(),
+        Product.countDocuments(filter),
       ]);
+
       return res.status(200).json({
         data: products,
         pagination: {
-          page,
-          pageSize,
+          page: parseInt(page),
+          pageSize: parseInt(pageSize),
           total,
           totalPages: Math.ceil(total / pageSize),
         },
       });
     } catch (error) {
+      console.log("Error getting products:", error);
       return res.status(500).json({ message: error.message });
     }
   },
