a5cb22f74c00d7883d187e59a8c28490651a3be7
mielindev <phamvietquang.dev@gmail.com>
2025-07-15 10:18:44 +0700

    🐛 fix(product): correct favorite product id check
    - update the condition to check favorite products using product._id
    - add breadcrumbs for better navigation

--- frontend/src/pages/main/ProductDetailPage.jsx
+++ frontend/src/pages/main/ProductDetailPage.jsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import CourseFeatures from "../../components/CourseFeatures";
 import StarRating from "../../components/StarRating";
 import useProductStore from "../../store/useProductStore";
@@ -16,8 +16,7 @@ const ProductDetailPage = () => {
 
   const isFavorited =
     optimisticFavorites.includes(id) ||
-    favorites.some((fav) => fav?.product === id);
-
+    favorites.some((fav) => fav?.product?._id === id);
   useEffect(() => {
     getProductById(id);
   }, [id, getProductById]);
@@ -51,67 +50,74 @@ const ProductDetailPage = () => {
     }
   };
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-2 w-full p-4 my-10 gap-6 bg-base-100 space-4 min-h-screen">
-      {/* Product Image */}
-      <div className="flex justify-center items-start rounded-lg overflow-hidden">
-        <img
-          src={productDetail?.image}
-          alt="Product Image"
-          className="lg:size-full lg:object-fit-cover rounded-lg"
-        />
+    <div>
+      <div className="breadcrumbs text-sm text-base-content/50 mx-5">
+        <ul>
+          <li>
+            <Link to={"/"}>Home</Link>
+          </li>
+          <li>
+            <Link to={"/products"}>Products</Link>
+          </li>
+          <li>Product Detail</li>
+        </ul>
       </div>
-
-      {/* Product Details */}
-      <div className="space-y-4 text-primary select-none">
-        <h1 className="text-4xl md:text-5xl font-semibold font-montserrat">
-          {productDetail?.name}
-        </h1>
-
-        {/* Rating */}
-        <div className="flex items-center space-x-2">
-          <StarRating rating={productDetail?.rating} />
-          <span className="text-base-content">
-            {productDetail?.rating} (453 reviews)
-          </span>
+      <div className="grid grid-cols-1 lg:grid-cols-2 w-full p-4  gap-6 bg-base-100 space-4 min-h-screen">
+        {/* Product Image */}
+        <div className="flex justify-center items-start rounded-lg overflow-hidden">
+          <img
+            src={productDetail?.image}
+            alt="Product Image"
+            className="lg:size-full lg:object-fit-cover rounded-lg"
+          />
         </div>
-
-        {/* Description */}
-        <p className="text-base-content/60">{productDetail?.description}</p>
-
-        {/* Price */}
-        <div className="space-y-3">
-          <p className="text-3xl tracking-wide font-bold">
-            {formatPrice(productDetail?.price)}
-          </p>
-          <p
-            className={`badge badge-outline text-md font-semibold ${getBadgeColor(
-              productDetail?.category.name
-            )}`}
-          >
-            {productDetail?.category.name}
-          </p>
+        {/* Product Details */}
+        <div className="space-y-4 text-primary select-none">
+          <h1 className="text-4xl md:text-5xl font-semibold font-montserrat">
+            {productDetail?.name}
+          </h1>
+          {/* Rating */}
+          <div className="flex items-center space-x-2">
+            <StarRating rating={productDetail?.rating} />
+            <span className="text-base-content">
+              {productDetail?.rating} (453 reviews)
+            </span>
+          </div>
+          {/* Description */}
+          <p className="text-base-content/60">{productDetail?.description}</p>
+          {/* Price */}
+          <div className="space-y-3">
+            <p className="text-3xl tracking-wide font-bold">
+              {formatPrice(productDetail?.price)}
+            </p>
+            <p
+              className={`badge badge-outline text-md font-semibold ${getBadgeColor(
+                productDetail?.category.name
+              )}`}
+            >
+              {productDetail?.category.name}
+            </p>
+          </div>
+          {/* Button */}
+          <div className="flex items-center space-x-4">
+            <button className="btn btn-primary rounded-full w-1/2 flex-1">
+              Shop Now
+            </button>
+            <button
+              className="btn btn-primary rounded-full"
+              onClick={() => {
+                handleFavorite(productDetail._id);
+              }}
+            >
+              <Heart
+                size={20}
+                fill={isFavorited ? "red" : "none"}
+                className={isFavorited ? "text-red-500" : "text-base-content"}
+              />
+            </button>
+          </div>
+          <CourseFeatures />
         </div>
-
-        {/* Button */}
-        <div className="flex items-center space-x-4">
-          <button className="btn btn-primary rounded-full w-1/2 flex-1">
-            Shop Now
-          </button>
-          <button
-            className="btn btn-primary rounded-full"
-            onClick={() => {
-              handleFavorite(productDetail._id);
-            }}
-          >
-            <Heart
-              size={20}
-              fill={isFavorited ? "red" : "none"}
-              className={isFavorited ? "text-red-500" : "text-base-content"}
-            />
-          </button>
-        </div>
-
-        <CourseFeatures />
       </div>
     </div>
   );
